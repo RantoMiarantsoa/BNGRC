38fcd637ee95c89d244267c749381d7c49b36ca2
MiaroYollan <miarorkt290906@gmail.com>
2026-02-16 18:53:40 +0300

    Synchronisation avant merge vers main

--- app/repositories/TypeBesoinRepository.php
+++ app/repositories/TypeBesoinRepository.php
@@ -12,15 +12,9 @@ class TypeBesoinRepository
     public function getAll(): array
     {
         $stmt = $this->db->query(
-<<<<<<< Updated upstream
-            'SELECT tb.id, tb.nom, c.nom as categorie, tb.prix_unitaire
-             FROM bngrc_type_besoin tb
-             LEFT JOIN bngrc_categorie c ON tb.categorie_id = c.id
-=======
             'SELECT tb.id, tb.nom, c.nom AS categorie, tb.prix_unitaire
              FROM bngrc_type_besoin tb
              LEFT JOIN bngrc_categorie c ON c.id = tb.categorie_id
->>>>>>> Stashed changes
              ORDER BY tb.nom ASC'
         );
 
