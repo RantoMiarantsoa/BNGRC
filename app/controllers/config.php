0bd44b8479dddb69834bf196aafc4252ad01854c
Arinaivo Andriatsihafa Ranto Miarantsoa <andriatsihafamiarantsoa@gmail.com>
2026-02-16 15:16:41 +0300

    petite erreur

--- app/controllers/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'bngrc');
-define('DB_USER', 'root'); // Remplacez par votre utilisateur PostgreSQL
-define('DB_PASS', '');         // Remplacez par votre mot de passe PostgreSQL
-define('DB_CHARSET', 'utf8');
-define('DB_DRIVER', 'mysql');
-
